72da04d91928ec5cad44bfc4a3c3df145802276b
Michael Rhodes <spam@michaelrhod.es>
2013-04-01 09:31:06 +1000

    Create huey.js

---
+++ huey.js
@@ -0,0 +1,53 @@
+Huey = (function(document, undefined) {
+
+	var canvas = null
+	var canvasSupported = (function(document) {
+		canvas = document.createElement("canvas")
+		return !!(canvas.getContext && canvas.getContext("2d"))
+	})(document)
+
+	var helper = {
+		getImageData: function(image) {
+			var context = canvas.getContext("2d")
+			canvas.width = image.width
+ 			canvas.height = image.height
+			context.drawImage(image, 0, 0)
+			return context.getImageData(0, 0, image.width, image.height).data
+		},
+		getColors: function(data) {
+			var colorCount = {}
+			var highestColorCount = 0
+			var mostFrequentColor = null
+			var rgb, min, max
+			for (var i = 0, l = data.length; i < l; i += 512) {
+				rgb = [data[i], data[i + 1], data[i + 2]]
+				min = Math.min(rgb[0], Math.min(rgb[1], rgb[2]))
+				max = Math.max(rgb[0], Math.max(rgb[1], rgb[2]))
+				if (max - min < 24)
+					continue
+				rgb = rgb.join(",")
+				colorCount[rgb] = (colorCount.hasOwnProperty(rgb) ? ++colorCount[rgb] : 1)
+			}
+			for (rgb in colorCount) {
+				if (colorCount[rgb] < highestColorCount) {
+					continue
+				}
+				highestColorCount = colorCount[rgb]
+				mostFrequentColor = rgb.split(",")
+			}
+			return mostFrequentColor
+		}
+	}
+
+	return function(url, callback) {
+		if (canvasSupported && url) {
+			var image = new Image()
+			image.onload = function() {
+				var data = helper.getImageData(image)
+				var color = helper.getColors(data, true)
+				callback(color)
+			}
+			image.src = url
+		}
+	}
+})(document)
