70f82f1333d93c8bdc0e264cc2ac3094765b6e43
michaelrhodes <spam@michaelrhod.es>
2019-02-05 21:34:45 +1000

    Add threshold argument

--- dominant.js
+++ dominant.js
@@ -2,27 +2,37 @@ var skip = require('./util/skip')
 
 module.exports = dominant
 
-function dominant (data) {
-  var count = {}
-  var max = 0
+function dominant (data, threshold) {
+  threshold = threshold == null ||
+    isNaN(threshold) ? 0 :
+    Number(threshold)
+
+  var highest = 0
   var winner = null
+  var count = {}
 
   var i = 0, l = data.length
-  var color, offset = skip(l)
+  var r, g, b, min, max, color
+  var offset = skip(l)
 
   for (; i < l; i += offset) {
-    color = hex(data[i]) +
-      hex(data[i + 1]) +
-      hex(data[i + 2])
+    r = data[i]
+    g = data[i + 1]
+    b = data[i + 2]
 
-    count[color] = (
-      count[color] || 0
-    ) + 1
+    if (threshold) {
+      min = Math.min(r, Math.min(g, b))
+      max = Math.max(r, Math.max(g, b))
+      if (max - min < threshold) continue
+    }
+
+    color = hex(r) + hex(g) + hex(b)
+    count[color] = (count[color] || 0) + 1
   }
 
   for (color in count) {
-    if (count[color] < max) continue
-    max = count[color]
+    if (count[color] <= highest) continue
+    highest = count[color]
     winner = color
   }
 
--- palette.js
+++ palette.js
@@ -3,16 +3,33 @@ var quantize = require('./util/quantize')
 
 module.exports = palette
 
-function palette (data, n) {
-  n = n == null || isNaN(n) ? 5 : +n
+function palette (data, n, threshold) {
+  n = n == null ||
+    isNaN(n) ? 5 :
+    Number(n)
+
+  threshold = threshold == null ||
+    isNaN(threshold) ? 0 :
+    Number(threshold)
 
   var i = 0, l = data.length
+  var r, g, b, min, max, color
   var offset = skip(l)
   var colors = []
 
-  for (; i < l; i += offset) colors.push([
-    data[i], data[i + 1], data[i + 2]
-  ])
+  for (; i < l; i += offset) {
+    r = data[i]
+    g = data[i + 1]
+    b = data[i + 2]
+
+    if (threshold) {
+      min = Math.min(r, Math.min(g, b))
+      max = Math.max(r, Math.max(g, b))
+      if (max - min < threshold) continue
+    }
+
+    colors.push([r, g, b])
+  }
 
   // Sometimes quantize is a weirdo and returns
   // more or less colors than you actually asked for…
